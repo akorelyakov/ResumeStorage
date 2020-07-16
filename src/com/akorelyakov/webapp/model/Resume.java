5f76be792fbb3a813a9856daabb2e585cef4427f
akorelyakov <al.korelyakov@gmail.com>
2020-07-16 12:21:04 +0300

    add DateUtil

--- src/com/akorelyakov/webapp/model/Resume.java
+++ src/com/akorelyakov/webapp/model/Resume.java
@@ -12,8 +12,8 @@ public class Resume implements Comparable<Resume> {
     private final Map<SectionType, AbstractSection> sections = new EnumMap<>(SectionType.class);
 
     public Resume(String uuid, String fullName) {
-        Objects.requireNonNull(uuid, "uuid must not be null)");
-        Objects.requireNonNull(fullName, "fullName must not be null)");
+        Objects.requireNonNull(uuid, "uuid must not be null");
+        Objects.requireNonNull(fullName, "fullName must not be null");
         this.uuid = uuid;
         this.fullName = fullName;
     }
